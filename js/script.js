65553c8a67946eb88ca35c1fffc35020cc6a3526
Callum Moseley <callum.moseley1@gmail.com>
2017-03-25 16:46:15 -0400

    added buildings

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 var propDist = 125;
 var propHeight = 183.15;
-var droneMaterial = new THREE.MeshPhongMaterial( { color: 0x303030, specular: 0x111111, shininess: 200 } );
+var droneMaterial = new THREE.MeshPhongMaterial({color: 0x303030, specular: 0x111111, shininess: 200});
+var buildingMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, specular: 0x111111, shininess: 200});
+var buildingSize = 10;
 
 var container, stats;
 
 var camera, controls, scene, renderer;
 var droneBodyGeom, dronePropRGeom, dronePropLGeom;
 var drones = [];
+// var webSocket = new WebSocket("ws://35.2.178.81:4567/map");
+var terrain = [];
 
 function Drone(p, scale, scene) {
-    this.flying = true;
+    this.name = "";
+
     this.droneGroup = new THREE.Group();
 
     this.droneBody = new THREE.Mesh( droneBodyGeom, droneMaterial );
@@ -58,32 +63,48 @@ function init() {
     var loader = new THREE.STLLoader();
     loader.load( 'models/DroneBody.stl', function ( geometry ) {
         droneBodyGeom = geometry;
-
-        loader.load( 'models/DronePropL.stl', function ( geometry ) {
-            dronePropLGeom = geometry;
-
-            loader.load( 'models/DronePropR.stl', function ( geometry ) {
-                dronePropRGeom = geometry;
-                drones.push(new Drone(new THREE.Vector3(0, 0, 0), 0.01, scene));
-            } );
-        } );
     } );
+
+    loader.load( 'models/DronePropL.stl', function ( geometry ) {
+        dronePropLGeom = geometry;
+    } );
+
+    loader.load( 'models/DronePropR.stl', function ( geometry ) {
+        dronePropRGeom = geometry;
+    } );
+
+    $.get('http://simonguo.tech/map.txt', function(data) {
+        var lines = data.split('\n');
+        for (var i = 0; i < lines.length; i++)
+        {
+            terrain.push([]);
+            var tiles = lines[i].split(' ');
+            for (var j = 0; j < tiles.length - 1; j++)
+            {
+                var tileData = tiles[j].split(',');
+                terrain[i].push([parseInt(tileData[0]), parseInt(tileData[1])]);
+            }
+        }
+        generateBuildings();
+    });
+
+    // webSocket.send("init");
 
     container = document.createElement( 'div' );
     document.body.appendChild( container );
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 1000 );
-    camera.position.set( 0, 5, -5 );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 600000 );
+    camera.position.set( 0, 50, -50 );
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x6699ff);
-    scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+    // scene.fog = new THREE.Fog( 0xffffff, 1, 4000 );
 
 
     // Ground
 
     var plane = new THREE.Mesh(
-        new THREE.PlaneBufferGeometry( 100, 100 ),
+        new THREE.PlaneBufferGeometry( 5000, 5000 ),
         new THREE.MeshPhongMaterial( { color: 0x559955, specular: 0x101010 } )
     );
     plane.rotation.x = -Math.PI/2;
@@ -94,18 +115,18 @@ function init() {
 
     // LIGHTS
 
-    hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
-    hemiLight.color.setHSL( 0.6, 1, 0.6 );
-    hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+    var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.4 );
+    hemiLight.color.setHSL( 0.6, 0, 0.6 );
+    hemiLight.groundColor.setHSL( 0.095, 0, 0.75 );
     hemiLight.position.set( 0, 500, 0 );
     scene.add( hemiLight );
 
     //
 
-    dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
     dirLight.color.setHSL( 0.1, 1, 0.95 );
     dirLight.position.set( -1, 1.75, 1 );
-    dirLight.position.multiplyScalar( 50 );
+    dirLight.position.multiplyScalar( 500 );
     scene.add( dirLight );
 
     dirLight.castShadow = true;
@@ -120,13 +141,13 @@ function init() {
     dirLight.shadow.camera.top = d;
     dirLight.shadow.camera.bottom = -d;
 
-    dirLight.shadow.camera.far = 3500;
+    dirLight.shadow.camera.far = 5000;
     dirLight.shadow.bias = -0.0001;
 
     // renderer
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setClearColor( scene.fog.color );
+    // renderer.setClearColor( scene.fog.color );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
 
@@ -137,10 +158,7 @@ function init() {
     renderer.shadowMap.renderReverseSided = false;
 
     controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.addEventListener( 'change', render ); // remove when using animation loop
-    // enable animation loop when using damping or autorotation
-    //controls.enableDamping = true;
-    //controls.dampingFactor = 0.25;
+    controls.addEventListener( 'change', render );
     controls.enableZoom = true;
 
     container.appendChild( renderer.domElement );
@@ -151,15 +169,39 @@ function init() {
     container.appendChild( stats.dom );
 
     window.addEventListener( 'resize', onWindowResize, false );
+
+    // webSocket.onmessage = function(message) {
+    //     var droneMessages = JSON.parse(message.data);
+    //     if (drones.length == 0) {
+    //         for (var i = 0; i < droneMessages.length; i++) {
+    //             drones.push(new Drone(new THREE.Vector3(droneMessages[i].x, droneMessages[i].y, droneMessages[i].z), 0.1, scene));
+    //         }
+    //     } else {
+    //         for (i = 0; i < droneMessages.length; i++) {
+    //             drones[droneMessages[i].id].droneGroup.position.set(droneMessages[i].x, droneMessages[i].y, droneMessages[i].z);
+    //         }
+    //     }
+    // }
+}
+
+function generateBuildings() {
+    for (var i = 0; i < terrain.length; i++) {
+        for (var j = 0; j < terrain.length; j++) {
+            var x = i * buildingSize - terrain.length / 2 * buildingSize;
+            var z = j * buildingSize - terrain.length / 2 * buildingSize;
+            var geometry = new THREE.BoxGeometry(buildingSize, terrain[i][j][1] * buildingSize, buildingSize);
+            var building = new THREE.Mesh(geometry, buildingMaterial);
+            building.position.set(x, 0, z);
+            scene.add(building);
+        }
+    }
 }
 
 function onWindowResize() {
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
 
 function animate() {
@@ -169,13 +211,10 @@ function animate() {
     controls.update();
 
     for (var i = 0; i < drones.length; i++) {
-        if (drones[i].flying)
-        {
-            drones[i].dronePropR1.rotateY(-0.4);
-            drones[i].dronePropR2.rotateY(-0.4);
-            drones[i].dronePropL1.rotateY(0.4);
-            drones[i].dronePropL2.rotateY(0.4);
-        }
+        drones[i].dronePropR1.rotateY(-0.4);
+        drones[i].dronePropR2.rotateY(-0.4);
+        drones[i].dronePropL1.rotateY(0.4);
+        drones[i].dronePropL2.rotateY(0.4);
     }
 
     render();
